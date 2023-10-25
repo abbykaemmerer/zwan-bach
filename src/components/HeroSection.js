2a436584eecf551b9aa9ee2f0ff49e14939a3a4e
abbykaemmerer <akaemmerer@vermeer.com>
2023-10-25 09:44:59 -0500

    10.25: desktop view for home page

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,25 +6,29 @@ import '../App.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
+      <div className='desktop-layout'>
         <h1 className='header'>
-          <span>Welcome</span>
-          <span>Campers</span> 
-        </h1>
-        <figure className='picture' >
-          <img
-              src='images/smokey-7.jpeg'
-              alt="Smokey Bear"
-              className='hero_img'
-          />
-        </figure>
-        <div className='information'>
-          <div>Lake Somewhere, Tennessee</div>
-          <div>May 17 - 19, 2023</div>
-        </div>
+            <span>Welcome</span>
+            <span>Campers.</span> 
+          </h1>
+          <figure className='picture' >
+            <img
+                src='images/smokey-7.jpeg'
+                alt="Smokey Bear"
+                className='hero_img'
+            />
+          </figure>
+          <div className='information'>
+            <div>Lake Grool, Tennessee</div>
+            <div>May 17 - 19, 2023</div>
+          </div>
+      </div>
+      <div className='bottom-div'>
         <div id='gif'>
-          <iframe src="https://giphy.com/embed/xT1R9PD10fWEQxdDOw" frameBorder="0" className="giphy-embed gif" title='baby-gif' allowFullScreen></iframe>
+            <iframe src="https://giphy.com/embed/xT1R9PD10fWEQxdDOw" frameBorder="0" className="giphy-embed gif" title='baby-gif' allowFullScreen></iframe>
         </div> 
-        <div className='babies-div'>It's us. We're the babies.</div>
+        <div className='babies-div'>It's us. We're the babes.</div>
+      </div>
     </div>
   );
 }
